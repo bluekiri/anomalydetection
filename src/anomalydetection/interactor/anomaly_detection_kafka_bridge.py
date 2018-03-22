addbe2e761d4a95df5662d7d5e8f195b76472bd8
jhuguet <juan.huguet@bluekiri.com>
2018-03-22 12:02:48 +0100

    [anomaly_detection_kafka_bridge]: map changed to sum to avoid conflicts with detector

--- src/anomalydetection/interactor/anomaly_detection_kafka_bridge.py
+++ src/anomalydetection/interactor/anomaly_detection_kafka_bridge.py
@@ -16,36 +16,21 @@ class AnomalyDetectionKafkaBridge:
 
         observable_to_predict = self.mean_by_windows(source_observable)
 
-        subject = self.map_output_result(observable=observable_to_predict).map(lambda item: self.test(item))
+        subject = self.map_output_result(observable=observable_to_predict)
         subject.subscribe(
             on_next=lambda item: self.kafka_repository.send_output_message(topic=output_topic, message_output=item))
 
     def map_output_result(self, observable: Observable) -> Subject:
-        observer = AnomalyDetectionKafkaBridge.AnomalyDetectionObserver(self.anomaly_detection_engine)
-        subject = Subject.create(observer=observer, observable=observable)
-
-        return subject
+        return observable.map(lambda item: {"application": item["application"],
+                                            "results_anomaly": self.anomaly_detection_engine.predict(item["value"]),
+                                            "value": item["value"]})
 
     def mean_by_windows(self, observable: Observable) -> Observable:
         def map_element(items):
             application = items[0].application
-            mean = sum(item.value for item in items) / len(items) if len(items) != 0 else 0
-            return {"application": application, "value": mean}
+            # mean = sum(item.value for item in items) / len(items) if len(items) != 0 else 0
+            sumation = sum(item.value for item in items)
+            return {"application": application, "value": sumation}
 
         return observable.buffer_with_time(timespan=self.time_in_seconds * 1000).filter(
             lambda items: len(items) > 0).map(map_element)
-
-    class AnomalyDetectionObserver(Observer):
-        def __init__(self, anomaly_detection_engine: BaseEngine):
-            self.anomaly_detection_engine = anomaly_detection_engine
-            super(AnomalyDetectionKafkaBridge.AnomalyDetectionObserver, self).__init__()
-
-        def on_next(self, value):
-            return self.anomaly_detection_engine.predict(value)
-
-        def on_error(self, error):
-            # TODO log error...
-            pass
-
-        def on_completed(self):
-            pass
