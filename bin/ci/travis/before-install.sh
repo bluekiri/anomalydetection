6ea8b38bee88aba94fcc14bd495b1092544778ae
Cristòfol Torrens <tofol.torrens@bluekiri.com>
2018-07-02 19:25:52 +0200

    Test fix

--- bin/ci/travis/before-install.sh
+++ bin/ci/travis/before-install.sh
@@ -5,6 +5,6 @@ openssl aes-256-cbc -K $encrypted_9e5a94a71a40_key -iv $encrypted_9e5a94a71a40_i
 
 # Install Spark
 cd /tmp
-wget http://www-eu.apache.org/dist/spark/spark-2.2.1/spark-2.2.1-bin-hadoop2.7.tgz -q --show-progress
+wget http://www-eu.apache.org/dist/spark/spark-2.2.1/spark-2.2.1-bin-hadoop2.7.tgz
 tar xzf spark-2.2.1-bin-hadoop2.7.tgz
 ln -s spark-2.2.1-bin-hadoop2.7 spark
